5a62475f1ec2594fd0adbb0d1f3f6dd88a66fc1c
Do Dac Thai Duong <ddtd281203@gmail.com>
2024-10-15 19:23:51 +0700

    Duong Push Login

--- NBStore-Be/routes/userRoutes.js
+++ NBStore-Be/routes/userRoutes.js
@@ -1,14 +1,9 @@
 const express = require('express');
-<<<<<<< HEAD
 const { getUsers, signin, createUser, changePassword, verifyEmail, resetAccount } = require('../controllers/userController');
-=======
-const { getUsers, createUser, changePassword, verifyEmail, resetAccount } = require('../controllers/userController');
->>>>>>> main
 const authenticateToken = require('../middleware/auth.middleware');
 const UserRouter = express.Router();
 
 UserRouter.get('/', getUsers);
-<<<<<<< HEAD
 UserRouter.post('/login', signin);
 UserRouter.post('/register', createUser);
 UserRouter.get('/verify-email', verifyEmail);
@@ -16,18 +11,3 @@ UserRouter.post('/resetPassword', resetAccount);
 UserRouter.post('/changepw', authenticateToken, changePassword);
 
 module.exports = UserRouter;
-=======
-UserRouter.post('/register', createUser);
-UserRouter.get('/verify-email', verifyEmail);
-UserRouter.post('/resetPassword', resetAccount)
-
-
-
-
-UserRouter.post('/changepw', authenticateToken, changePassword);
-
-
-
-
-module.exports = UserRouter;
->>>>>>> main
